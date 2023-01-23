36f8f9b3134cc25f3aa0b2c588f00502ce416c4c
Mohammed Al-jaberi <aljabri606@gmail.com>
2023-01-23 18:58:02 +0300

    completed

---
+++ index.js
@@ -0,0 +1,24 @@
+// let number1 = 4 ; 
+// let number2 = 2 ; 
+
+function add(num1,num2) {
+  return (num1+= num2);
+} ;
+
+function divide(num1,num2) {
+  return (num1 /= num2 );
+} ;
+function multiply(num1,num2) {
+  return (num1 *= num2 );
+} ;
+function substract(num1,num2) {
+  return (num1 /= num2 );
+} ;
+ console.log(add(4,2))
+ console.log(multiply(4,2))
+ console.log(divide(4,2))
+ console.log(substract(4,2))
+ console.log(parseInt("2.2222"));
+ console.log(parseFloat("80"));
+ console.log(parseFloat("80"));
+ console.log(preserveDecimal("80"));
